15fc1a1e710622ad1b0c0ca4e6aa392aa4ffa308
Abhishek Lolage <abhisheklolage@gmail.com>
2019-04-29 13:44:13 -0600

    feature dumps in numpy format for sketches

--- datasets/caltech256.py
+++ datasets/caltech256.py
@@ -2,7 +2,7 @@
 
 # path example: caltech256/219.theodolite/219_0010.jpg
 import os
-import datautils
+from datasets import datautils
 
 def get_images(QUERY):
     '''
@@ -12,7 +12,7 @@ def get_images(QUERY):
     '''
     labels           = []
     dataset          = []
-    image_paths      = list(datautils.list_images("caltech256"))
+    image_paths      = list(datautils.list_images("./datasets/caltech256"))
     stats = {}
     for (i, image_path) in enumerate(image_paths):
         label = int(image_path.split(os.path.sep)[-1].split("_")[0])
@@ -22,12 +22,10 @@ def get_images(QUERY):
             stats[label] += 1
         dataset.append([image_path, label])
     dataset = sorted(dataset, key = lambda x: x[1])
-
     training_images = [[]]
     query_images    = [[]]
     dataset_ptr     = 0
     for cat in range(1, 258): # 257 labels
-        print("Splitting category#", cat)
         num_queries = int(stats[cat] * QUERY)
 
         start = dataset_ptr
@@ -41,11 +39,8 @@ def get_images(QUERY):
 
 
     for cat in range(1, 258):
-        print("Checking for category#", cat)
-        print(len(query_images[cat]), len(training_images[cat]), stats[cat])
+        #print(len(query_images[cat]), len(training_images[cat]), stats[cat])
         assert(len(query_images[cat]) + len(training_images[cat]) == stats[cat])
     print("Splitting done")
 
     return training_images, query_images
-
-training, query = get_images(0.10)
--- pretrained.py
+++ pretrained.py
@@ -1,17 +1,59 @@
+#
+# This code is for converting images to embeddings, using pretrained and trained
+# models
+#
+
 from keras.preprocessing import image
-from keras.applications.vgg16 import VGG16
-from keras.applications.vgg16 import preprocess_input
+from keras.applications.resnet50 import ResNet50
+from keras.applications.resnet50 import preprocess_input
+from keras import Model
+from datasets import caltech256
+from datasets import datautils
 import numpy as np
 
-model = VGG16(weights='imagenet', include_top=False)
-model.summary()
+def get_embedding(model, img_path, layer_name):
 
-img_path = ''
-img = image.load_img(img_path, target_size=(224, 224))
-img_data = image.img_to_array(img)
-img_data = np.expand_dims(img_data, axis=0)
-img_data = preprocess_input(img_data)
+  return embedding
 
-vgg16_feature = model.predict(img_data)
 
-print vgg16_feature.shape
+## get dataset splits
+
+training_paths, query_paths = caltech256.get_images(0.10)
+print("DONE")
+
+## training code, get model
+## pretrained model
+model = ResNet50(weights='imagenet')
+
+##
+training_feature_vectors = []
+query_feature_vectors = []
+
+PATH = 0
+CAT  = 1
+intermediate_layer_model = Model(inputs=model.input, \
+                                 outputs=model.get_layer('avg_pool').output)
+for category in range(1, 258):
+    ctr  = 0
+    print("Starting category #", category)
+    for im in training_paths[category]:
+        img = image.load_img(im[0], target_size=(224, 224))
+        img_data = image.img_to_array(img)
+        img_data = np.expand_dims(img_data, axis=0)
+        img_data = preprocess_input(img_data)
+        emb = intermediate_layer_model.predict(img_data)
+        np_emb = np.array(emb).flatten()
+        training_feature_vectors.append(np_emb)
+        ctr += 1
+    for im in query_paths[category]:
+        img = image.load_img(im[0], target_size=(224, 224))
+        img_data = image.img_to_array(img)
+        img_data = np.expand_dims(img_data, axis=0)
+        img_data = preprocess_input(img_data)
+        emb = intermediate_layer_model.predict(img_data)
+        np_emb = np.array(emb).flatten()
+        query_feature_vectors.append(np_emb)
+        ctr += 1
+    print("# images in cat", category, "=", ctr)
+
+np.savez("feature-dump.npz", emb_training = training_feature_vectors, emb_query = query_feature_vectors)
